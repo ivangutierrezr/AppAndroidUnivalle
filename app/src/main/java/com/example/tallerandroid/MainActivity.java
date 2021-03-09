79bde6a06bbfe758a5b245c57eaf187e2daa49d4
ivangutierrezr <ivangutierrezr@gmail.com>
2021-03-09 15:57:57 -0500

    app update

--- app/src/main/java/com/example/tallerandroid/MainActivity.java
+++ app/src/main/java/com/example/tallerandroid/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Switch swAuth;
     AdView mAdView;
     TextView errorText;
+    EditText edHello;
     Boolean accessAllowed = false;
     Object users;
     String usersArray;
@@ -90,6 +91,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        myRef.setValue("Profesor Royer");
+//        edHello = findViewById(R.id.edHello);
+
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                edHello.setText(" " + value );
+//                //  Log.d(TAG, "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                //  Log.w(TAG, "Failed to read value.", error.toException());
+//            }
+//        });
     }
 
     private void signIn() {
--- app/src/main/java/com/example/tallerandroid/MyLists.java
+++ app/src/main/java/com/example/tallerandroid/MyLists.java
@@ -12,6 +12,14 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +28,11 @@ public class MyLists extends AppCompatActivity {
     ListView listofthings;
     String username;
     String password;
+
+    Object users;
+    String usersArray;
+    private String url1 = "https://invessoft.com/api/eventos/2";
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +60,68 @@ public class MyLists extends AppCompatActivity {
                 your_array_list );
 
         listofthings.setAdapter(arrayAdapter);
+    }
+
+    public void getdb(View l) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url1, null, response -> {
+                    try {
+                        users = response.get("agenda");
+                        System.out.println(users);
+                        usersArray = users.toString();
+                        System.out.println(usersArray);
+
+                        JSONArray array = new JSONArray(usersArray);
+                        for (int i =0; i < array.length(); i++) {
+                          /*  "id_trabajo_agenda": 619,
+                                "fecha": "2020-08-21",
+                                "hora_inicio": "10:00:00",
+                                "hora_fin": "10:20:00",
+                                "id_espacio": 46,
+                                "id_trabajo":*/
+                            JSONObject row = array.getJSONObject(i);
+                            String id_trabajo_agenda = row.getString("id_trabajo_agenda");
+                            String fecha = row.getString("fecha");
+                            String hora_inicio = row.getString("hora_inicio");
+                            String hora_fin = row.getString("hora_fin");
+                            String id_espacio = row.getString("id_espacio");
+                            String id_trabajo = row.getString("id_trabajo");
+
+                            System.out.println("id_trabajo_agenda: ");
+                            System.out.println(id_trabajo_agenda);
+                            System.out.println("fecha: ");
+                            System.out.println(fecha);
+                            System.out.println("hora_inicio: ");
+                            System.out.println(hora_inicio);
+                            System.out.println("hora_fin: ");
+                            System.out.println(hora_fin);
+                            System.out.println("id_espacio: ");
+                            System.out.println(id_espacio);
+                            System.out.println("id_trabajo: ");
+                            System.out.println(id_trabajo);
+
+//                            private static final String SQL_CREATE_ENTRIES =
+//                                    "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+//                                            FeedEntry._ID + " INTEGER PRIMARY KEY," +
+//                                            FeedEntry.COLUMN_NAME_TITLE + " TEXT," +
+//                                            FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
+//
+//                            private static final String SQL_DELETE_ENTRIES =
+//                                    "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
+
+                        }
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, error -> {
+                    // TODO: Handle error
+
+                });
+
+        requestQueue.add(jsonObjectRequest);
+
     }
 
     public void logout(View l) {
